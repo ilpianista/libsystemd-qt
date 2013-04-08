78b064c58779d930a6115de7f9b7bcca9781df92
Andrea Scarpino <andrea@archlinux.org>
2013-04-08 16:24:08 +0200

    Fix license

--- dbus/generic-types.h
+++ dbus/generic-types.h
@@ -1,5 +1,5 @@
-/***************************************************************************
- * Copyright (c) 2013 Andrea Scarpino <andrea@archlinux.org>
+/*
+ * Copyright (C) 2013  Andrea Scarpino <me@andreascarpino.it>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -8,14 +8,14 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the
- * Free Software Foundation, Inc.,
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- ***************************************************************************/
+ *
+ */
 
 #ifndef GENERIC_TYPES_H
 #define GENERIC_TYPES_H
